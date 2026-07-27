d61d384075c382a95bcee5a558422a6a621d4e38
alibabadoust <133060013+alibabadoust@users.noreply.github.com>
2026-07-27 20:10:37 +0300

    The 4th question of while loop and I could solve it

---
+++ Reeborgs_Robot_WhileLoop_Hardestformat.py
@@ -0,0 +1,23 @@
+# def turn_around():
+#     turn_left()
+#     turn_left()
+#     turn_left()   
+# def jump():
+#     turn_left()
+#     while wall_on_right():
+#            move()
+#     turn_around()
+#     move()
+#     turn_around()
+#     while not wall_in_front():
+#          move()
+           
+#     turn_left()
+  
+# while   not at_goal():
+#     if wall_in_front():
+#          jump()
+#     else:
+#         move()
+#I should have learned that think clearly and simply and pay much attention to what problem
+#says  to me .and I could solve this problem in my second try .
