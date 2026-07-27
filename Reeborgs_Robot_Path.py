d52e5028ffb01a543750633fe948d14344a1d044
alibabadoust <133060013+alibabadoust@users.noreply.github.com>
2026-07-27 15:50:43 +0300

    the robo game

---
+++ Reeborgs_Robot_Path.py
@@ -0,0 +1,17 @@
+I used  pre-made functions (turn_left()and move())
+#three times turn to left make the robot to turn_right
+def turn_around():
+    turn_left()
+    turn_left()
+    turn_left()   
+def jump():
+    turn_left()
+    move()
+    turn_around()
+    move()
+    turn_around()
+    move()
+    turn_left()
+for x in range(0,6):
+    move()
+    jump()
