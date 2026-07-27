0abf14d751383819611fef72a5f3addcae8520cb
alibabadoust <133060013+alibabadoust@users.noreply.github.com>
2026-07-27 19:51:17 +0300

    the second while loop ,it teaches me that I should think in simple way

---
+++ Reeborgs_Robot_While.py
@@ -0,0 +1,22 @@
+# def turn_around():
+#     turn_left()
+#     turn_left()
+#     turn_left()   
+# def jump():
+#     turn_left()
+#     move()
+#     turn_around()
+#     move()
+#     turn_around()
+#     move()
+#     turn_left()
+  
+# while   not at_goal():
+#     if wall_in_front():
+#          jump()
+#     else:
+#         move()
+#the problem is so simple you should just think in simple way do not make 
+# it harder.just think with these functions you have ,how can you choose the problem.
+#if there is wall in front of it,it should be jump but if there is not any wall in front of it 
+#then it can move directly.
